bfc4058688ddb457188fd50fe75d98d1eb5c7cd9
Raviz1 <72331994+Raviz1@users.noreply.github.com>
2021-01-26 13:36:47 +0100

    DONE

---
+++ 3.Operatory/script.js
@@ -0,0 +1,51 @@
+// Zadanie 1
+
+let x = true,
+	  y = false;
+
+console.log(x==y);
+
+// Zadanie 2
+
+let x = 15;
+let y = 30;
+let moduloResult = 0;
+moduloResult = x%y;
+console.log(moduloResult)
+
+// Zadanie 3 
+
+let name = "Adrian";
+let secondName = " Ścibisz";
+let stringsResult;
+
+stringsResult = name + secondName;
+
+console.log(stringsResult);
+
+// Zadanie 4
+
+var someNumber = 425;
+var someString = "425"
+
+console.log(someNumber==someString);
+console.log(someNumber===someString);
+
+// Zadanie 5
+
+let counter = 30;
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+// Zadanie 6
+
+let x = 40;
+let y = 20;
+
+let result = null;
+
+result = x > y;
+
+console.log(result);
