249e07b6f9a58441258f641889d31a584f8044f9
Raviz1 <72331994+Raviz1@users.noreply.github.com>
2021-01-26 13:15:31 +0100

    DONE

---
+++ 1.Początki/script.js
@@ -0,0 +1,15 @@
+//Zadanie 1
+
+console.log('Adrian Ścibisz');
+
+//Zadanie 2
+
+console.log(12 + 32); //44
+console.log(52 / 16); //3.25
+console.log(5 * 2);   //10
+
+//Zadanie 3
+
+console.log("Hello world"); //Wyświetla sie string Hello world.
+console.log(Hello World);   //Error brak przecinka.
+console.log("Hello World);  //Brakuje domkniętego stringa.
