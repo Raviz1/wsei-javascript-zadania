edcdcdc404b45e0bef65b5644784d7a67a56ae32
Raviz1 <72331994+Raviz1@users.noreply.github.com>
2021-01-26 13:25:46 +0100

    DONE

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,29 @@
+// Zadanie 1
+
+const	num = 3,	
+str = '4',				
+combo = num + str,		
+bool = true,			
+nothing = null;
+
+console.log(num);				
+console.log(str);				
+console.log(combo);
+console.log(bool);
+console.log(nothing);
+
+// Zadanie 2:
+
+let	num = 8,
+num1 = 4,
+sum = 0;					
+
+sum = num + num1;
+
+console.log(sum);
+
+// Zadanie 3
+
+let num;						
+
+console.log(num);
